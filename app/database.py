a4d431ae8cacc06fb096081170cf67c50fb76acf
daniyar-udel <daniyar@udel.edu>
2026-06-01 18:44:01 -0400

    Docker

--- app/database.py
+++ app/database.py
@@ -14,7 +14,7 @@ class Base(DeclarativeBase):
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:admin@localhost:5432/ecommerce_db"
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:admin@db:5432/ecommerce_db"
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
 
